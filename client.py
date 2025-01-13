ba1713dc58b9987f8ec84875d4f28e1006c1bf62
CryptonicAnon <hunter.stahl@us.af.mil>
2025-01-13 11:40:27 -0600

    Added basic try: for client side

--- client.py
+++ client.py
@@ -1,7 +1,21 @@
 import socket
 
 def main():
-    host = '127.0.0.1'
+    host = '' # Server's host address
     port = 22000
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to the server
+        client_socket.connect((host, port))
+        print(f"Connected to {host}:{port}")
+
+        # Continue here for what the client is sending to the server.
+    finally:
+        # Close the connection
+        client_socket.close()
+        print("Connection closed.")
+
+if __name__ == "__main__":
+    main()
