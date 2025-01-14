2b102d50d40a77a1de2f2a7e6e029bbf9afc9643
eckshawn <shawn.eck00@gmail.com>
2025-01-14 11:22:49 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 import socket
 
 BUFFERSIZE=1024
-
 HOST = socket.gethostname() # Server's host address
 PORT = 22000
 
 def main():
     continue_game = True
+    score = 0
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,16 +14,6 @@ def main():
         # Connect to the server
         client_socket.connect((HOST, PORT))
         print(f"Connected to {HOST}:{PORT}")
-
-        # Send multiple messages to the server
-        for i in range(3):
-            message = f"Message {i+1} from client"
-            client_socket.send(message.encode())
-            print(f"Sent: {message}")
-
-            # Receive a response
-            response = client_socket.recv(1024).decode()
-            print(f"Server responded: {response}")
 
         while(continue_game):
             question = client_socket.recv(1024).decode()
@@ -36,8 +26,12 @@ def main():
 
             if answer == correct_answer:
                 print("You got the question right!")
+                score += 1
             elif answer != correct_answer:
-                print("You got the question wrong")
+                print("You lost GG lbitchoser.")
+                client_socket.close()
+                print("Connection closed.")
+                quit()
             else:
                 print("this didnt work")
 
