8014d03917415610ce1ae084ca5afea58bd1cf1e
CryptonicAnon <hunter.stahl@us.af.mil>
2025-01-14 09:07:46 -0600

    server changes

--- server.c
+++ server.c
@@ -6,3 +6,16 @@
 #include <pthread.h>
 
 #define PORT 22000
+#define BUFFER 1024
+
+int main()
+{
+    int server_fd;
+
+    // Create the server socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+}
